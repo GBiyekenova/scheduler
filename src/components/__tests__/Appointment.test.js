7c57be9ce99a67251c32348f038fad4a772bb7f4
Gulbike Biyekenova <gbknva@gmail.com>
2021-12-15 17:23:23 -0500

    writes tests for Form component

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Application from "components/Application";
+
+afterEach(cleanup);
+
+describe("Appointment", () => {
+  afterEach(() => {
+    return <Application />
+  });
+  it("renders without crashing", () => {
+    render(<Application />);
+
+  })
+});
