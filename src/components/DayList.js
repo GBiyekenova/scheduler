3f17b443f44c1fb49c8cfdfd5f95868f1d6cc76d
Gulbike Biyekenova <gbknva@gmail.com>
2021-12-15 22:44:09 -0500

    adds a prop to pass cypress tests

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,11 +2,12 @@ import React from 'react';
 import DayListItem from './DayListItem';
 
 export default function DayList(props) {
-  console.log(props);
+  //console.log(props);
   const parsedDays = props.days.map(day => {
+    //console.log(day);
   return (
   <DayListItem 
-  key={props.days.id} 
+  key={day.id} 
   setDay={props.onChange} 
   selected={day.name === props.value} 
   {...day} 
